2b9bc92288c05a932ab3f1a6bb66c67d9a842bbb
Proust Sébastien <sebastienproust77@gmail.com>
2022-03-07 17:34:20 +0100

    Partie accueil CSS

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const btnMenu = document.querySelector(".btn-rond-menu");
-const nav = document.querySelector(".nav-gauche");
-const allItemNav = document.querySelectorAll(".nav-menu-item");
-const ligne = document.querySelector(".cont-ligne");
+const btnMenu = document.querySelector('.btn-rond-menu')
+const nav = document.querySelector('.nav-gauche');
+const allItemNav = document.querySelectorAll('.nav-menu-item');
+const ligne = document.querySelector('.cont-ligne');
 
-btnMenu.addEventListener("click", () => {
-  ligne.classList.toggle("active");
-  nav.classList.toggle("menu-visible");
-});
+btnMenu.addEventListener('click', () => {
 
-if (window.matchMedia("(max-width: 1300px)")) {
-  allItemNav.forEach((item) => {
-    item.addEventListener("click", () => {
-      nav.classList.toggle("menu-visible");
-      ligne.classList.toggle("active");
-    });
-  });
+    ligne.classList.toggle('active')
+    nav.classList.toggle('menu-visible')
+
+})
+
+if(window.matchMedia('(max-width: 1300px)')) {
+ 
+    allItemNav.forEach(item => {
+        item.addEventListener('click', () => {
+            nav.classList.toggle('menu-visible')
+            ligne.classList.toggle('active');
+        })
+    })
+
 }
